011cf4b8d0f5a9e4031c326a3fe275de40558d0b
Boka <boris.enuqashvili@gmail.com>
2023-02-08 12:25:10 +0400

    max

--- Example005_HelloUser/Program.cs
+++ Example005_HelloUser/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя:");
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ураа, это же Маша!");
 }
