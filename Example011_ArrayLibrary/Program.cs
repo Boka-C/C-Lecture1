4feea2792920b311425e08e67758b6299ee02144
Boka <boris.enuqashvili@gmail.com>
2023-02-08 20:27:31 +0400

    массив определенной длинный, заполненный рандомными числами

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; // определили массив с указанием его длинны 
+
+FillArray(array); // заполнили массив рандомными? числами
+PrintArray(array); // вывели на экран 
