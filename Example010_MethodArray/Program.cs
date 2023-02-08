bf9ca23cc8a905feb820698ea256c54263cfc083
Boka <boris.enuqashvili@gmail.com>
2023-02-08 19:52:53 +0400

    поиск значения в нумерации массива

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 31, 4, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index); // нашли число файнд (4), в массиве под номером 3 (нумерация с 0)
+    }
+    //index = index + 1; то же самое что и в слю строке
+    index++; 
+
+}
